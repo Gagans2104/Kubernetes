d86176bb205e88fcf607875b225f8e462254930e
Gagan2104 <gagan7876@gmail.com>
2023-09-02 18:18:37 +0530

    New Commit2

--- src/main/java/com/learning/kubernetes/controller/KubeController.java
+++ src/main/java/com/learning/kubernetes/controller/KubeController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KubeController {
 
-    @GetMapping
+    @GetMapping("/hi")
     public String sayHi(){
         return "Hi";
+    }
+    @GetMapping("/bye")
+    public String sayBye(){
+        return "Bye";
     }
 }
